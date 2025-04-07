54f4820d3912617cb9da80c4b253045ad6f73a33
Lewan <dariusz.lewandowski2009@outlook.de>
2025-04-07 10:02:24 +0200

    First Changes

---
+++ src/main/java/net/dlmspielt/betteroreprogression/BetterOreProgressionClient.java
@@ -0,0 +1,10 @@
+package net.dlmspielt.betteroreprogression;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BetterOreProgressionClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
