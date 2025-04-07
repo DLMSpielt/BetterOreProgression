e03bc098ff9fab994d488e7e340e0e0bdef88557
Lewan <dariusz.lewandowski2009@outlook.de>
2025-04-07 14:15:39 +0200

    Enderite Ingot added

--- src/main/java/net/dlmspielt/betteroreprogression/BetterOreProgression.java
+++ src/main/java/net/dlmspielt/betteroreprogression/BetterOreProgression.java
@@ -1,5 +1,6 @@
 package net.dlmspielt.betteroreprogression;
 
+import net.dlmspielt.betteroreprogression.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,5 +12,6 @@ public class BetterOreProgression implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.initialize();
 	}
 }
--- src/main/java/net/dlmspielt/betteroreprogression/item/ModItems.java
+++ src/main/java/net/dlmspielt/betteroreprogression/item/ModItems.java
@@ -1,7 +1,6 @@
 package net.dlmspielt.betteroreprogression.item;
 
 import net.dlmspielt.betteroreprogression.BetterOreProgression;
-import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -12,7 +11,6 @@ import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 import org.spongepowered.include.com.google.common.base.Function;
 
-//enderite Items from netherite 35 hue!!!
 public class ModItems {
     public static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
         // Create the item key.
@@ -26,12 +24,8 @@ public class ModItems {
 
         return item;
     }
-
-    public static final Item ENDERITE_INGOT = register("enderite_ingot", Item::new, new Item.Settings());
-
-    public static void initialize() {// Get the event for modifying entries in the ingredients group.
-// And register an event handler that adds our suspicious item to the ingredients group.
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
-                .register((itemGroup) -> itemGroup.add(ModItems.ENDERITE_INGOT));
+public static final Item ENDERITE_INGOT = register("enderide_ingot", Item :: new, new Item.Settings());
+    public static void initialize() {
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register((itemGroup) -> itemGroup.add(ModItems.ENDERITE_INGOT));
     }
 }
