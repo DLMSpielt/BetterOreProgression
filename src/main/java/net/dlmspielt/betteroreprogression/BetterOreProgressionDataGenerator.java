a86ea0a4bafc65923943692544404367e6aeccbb
Lewan <dariusz.lewandowski2009@outlook.de>
2025-04-07 09:20:47 +0200

    Initial commit

---
+++ src/main/java/net/dlmspielt/betteroreprogression/BetterOreProgressionDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dlmspielt.betteroreprogression;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterOreProgressionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
